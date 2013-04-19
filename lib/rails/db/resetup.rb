2efab1e28a2ece22a7e8fec51181ffb0419dea71
J.Fukaya <fukajun.shark@gmail.com>
2013-04-19 18:39:04 +0900

    init

---
+++ lib/rails/db/resetup.rb
@@ -0,0 +1,9 @@
+require "rails/db/resetup/version"
+
+module Rails
+  module Db
+    module Resetup
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/db/resetup/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Db
+    module Resetup
+      VERSION = "0.0.1"
+    end
+  end
+end
